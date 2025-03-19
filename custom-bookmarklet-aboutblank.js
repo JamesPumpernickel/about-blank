cbeafb63f06894e2472f592f608e935e55ea9548
JamesPumpernickel <skibidijames2@outlook.com>
2025-03-19 10:49:51 -0500

    Update and rename bookmarklet-aboutblank.js to custom-bookmarklet-aboutblank.js

---
+++ custom-bookmarklet-aboutblank.js
@@ -0,0 +1,2 @@
+javascript:(function(){var win=window.open();var url="ENTER CUSTOM URL HERE";if(url){var iframe=document.createElement("iframe");iframe.style.border="none";iframe.style.width="100%";iframe.style.height="100%";iframe.style.margin="0";iframe.src=url;win.document.body.appendChild(iframe);}})();
+//change "ENTER CUSTOM URL HERE" to the url you want
