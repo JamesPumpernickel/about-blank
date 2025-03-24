fdbffa8814b7049bcf9ebdcb2517b2d6eb363015
JamesPumpernickel <skibidijames2@outlook.com>
2025-03-24 08:04:40 -0500

    Update and rename custom-bookmarklet-aboutblank.js to bookmarklet-aboutblank.js

---
+++ bookmarklet-aboutblank.js
@@ -0,0 +1,17 @@
+javascript:(function() {
+    var url = "about:blank";
+    var src = window.location.href; // Use the current page URL
+    var win = window.open(url);
+    if (win) {
+        win.focus();
+        win.document.body.style.margin = '0';
+        win.document.body.style.height = '100vh';
+        var iframe = win.document.createElement('iframe');
+        iframe.style.border = 'none';
+        iframe.style.width = '100%';
+        iframe.style.height = '100%';
+        iframe.style.margin = '0';
+        iframe.src = src;
+        win.document.body.appendChild(iframe);
+    }
+})();
